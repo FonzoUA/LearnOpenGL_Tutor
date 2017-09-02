8673141c0b4077fb1180681c520d96f7ded42abb
Vadym Yanchuk <fonzoua@gmail.com>
2017-09-01 23:00:13 -0500

    Comment Fix
    It looked much nicer in VB

--- main.cpp
+++ main.cpp
@@ -52,43 +52,43 @@ int main()
 {
 	// ---------- Initialize and configure GLFW ----------
 	glfwInit();																	// Initializes GLFW
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);								// Configures GLFW. First parameter tells an option we want to configure and then comes selection
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);								// from enum of possible options prefixed with 'GLFW_'. Second argument is an int that sets the value of our option
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);												// Configures GLFW. First parameter tells an option we want to configure and then comes selection
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);												// from enum of possible options prefixed with 'GLFW_'. Second argument is an int that sets the value of our option
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);				// In this case, its specified that GLFW should use 3.3 version of OpenGL and that it should use core-profile
 
 	// ---------- GLFW window creation ----------
 	// Create window object that holds all the windowing data and used by other GLFW functions
 	GLFWwindow* pWindow = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Test Window", nullptr, nullptr);		// Create window of specified width and height 
-	if (pWindow == nullptr)																				// Check if creation was successful
+	if (pWindow == nullptr)																									// Check if creation was successful
 	{
 		printf("Failed to create GLFW window \n");														// If not, clear used resources, display error message and stop the program
 		glfwTerminate();
 		return -1;
 	}
-	glfwMakeContextCurrent(pWindow);																	// Make the context of the window the main context on the current thread
+	glfwMakeContextCurrent(pWindow);																				// Make the context of the window the main context on the current thread
 
 	// ---------- GLAD: Load all OpenGL function pointers ----------
 	// Since GLAD manages function pointers for OpenGL, initialize GLAD before calling any OpenGL functions
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))											// Pass GLAD the function to load address of the OpenGL function pointers
-	{																									// glfwGetProtAddress() defines the correct function based on which OS the program is compiled
+		{																																						// glfwGetProtAddress() defines the correct function based on which OS the program is compiled
 		printf("Failed to initialize GLAD\n");
 		return -1;
 	}
 
 	// ---------- Build and Compile shader program ----------
 	// Vertex shader 
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);												// Create a shader object
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);											// Create a shader object
 	glShaderSource(vertexShader, 1, &VertexShaderSource, nullptr);										// Attach shader source code to the shader object 
-																										// [Parameters] First: shader to compile. Second: Specify how many strings are being passed
-																										// as a source code. Third: actual source code reference. 
+																																										// [Parameters] First: shader to compile. Second: Specify how many strings are being passed
+																																										// as a source code. Third: actual source code reference. 
 	glCompileShader(vertexShader);																		// Compile the shader
 																										/*  First create an empty shader object and assign its ID to unsigned int (done with 'glCreateShader()'
 																											Since vertex shader is being created, pass in GL_VERTEX_SHADER as parameter. Next attach the
 																											shader source code to the shader object and compile the shader*/
 	
 	// Check for compilation errors
-	int success;																						// Indicates success status
-	char infoLog[512];																					// Stores error messages
+	int success;																																	// Indicates success status
+	char infoLog[512];																														// Stores error messages
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);											// Checks if compilation was successful
 	if (!success)																						
 	{	
@@ -141,27 +141,27 @@ int main()
 
 	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);														// Generate Vertex Array Object and assing ID to it
-	glGenBuffers(1, &VBO);															// Generate Vertex Buffer Object and assing ID to it
+	glGenBuffers(1, &VBO);																// Generate Vertex Buffer Object and assing ID to it
 
 	// Bind the Vertex Array Object first, then bind and set vertex buffers and then configure vertex attributes
 	glBindVertexArray(VAO);									
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);												// Bind buffer object to the current buffer type target
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);																						// Bind buffer object to the current buffer type target
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);		// Allocates memory and stores data within the initialized memory in the currently bound buffer object
-																					// [Parameters] First: Type of buffer we want to copy data into. Second: size of data (in bytes).
-																					// Third: data we want to send; Fourth: specifies how we want the graphics card to manage the given data
+																																								// [Parameters] First: Type of buffer we want to copy data into. Second: size of data (in bytes).
+																																								// Third: data we want to send; Fourth: specifies how we want the graphics card to manage the given data
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);	// Specifies how OpenGL should interpret the vertex buffer data whenever a drawing call is made.
-																					// [Parameters] First: Specify which vertex attribute to configure. Second: Specify size of the vertex attribute
-																					// Third: Specify type of data. Fourth: Whether data should be normalized. Fifth: Specify stride (space b/w consecutive
-																					// vertex arrtibute sets. Sixth: Specify offset of the begin position data
-	glEnableVertexAttribArray(0);													// Enables generic vertex attribute, giving location as its argument (disabled by default)
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);												// Unbind buffer. Since glVertexAttribPointer call registered VBO as the vertex attribute's bound VBO, its safe to unbind
-
-	glBindVertexArray(0);															// Unbind VAO
-
-#if WIREFRAME
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);										// Draws in wireframe polygons
+																																								// [Parameters] First: Specify which vertex attribute to configure. Second: Specify size of the vertex attribute
+																																								// Third: Specify type of data. Fourth: Whether data should be normalized. Fifth: Specify stride (space b/w consecutive
+																																								// vertex arrtibute sets. Sixth: Specify offset of the begin position data
+	glEnableVertexAttribArray(0);																									// Enables generic vertex attribute, giving location as its argument (disabled by default)
+								
+	glBindBuffer(GL_ARRAY_BUFFER, 0);																							// Unbind buffer. Since glVertexAttribPointer call registered VBO as the vertex attribute's bound VBO, its safe to unbind
+								
+	glBindVertexArray(0);																													// Unbind VAO
+								
+#if WIREFRAME								
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);																		// Draws in wireframe polygons
 #endif
 
 	// ---------- Render Loop ----------
@@ -172,18 +172,18 @@ int main()
 
 		// Render 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);											// Clear color buffer and set specific color to it at the same time
-		glClear(GL_COLOR_BUFFER_BIT);													// Specify which buffer we want to clean
+		glClear(GL_COLOR_BUFFER_BIT);															// Specify which buffer we want to clean
 
-																						// Draw Triangle
+		// Draw Triangle
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// GLFW: Swap buffers and poll IO events (keys pressed/released, mouse movement, etc)
-		glfwSwapBuffers(pWindow);														// Swaps the color buffer (large buffer that contains color values for each pixel in GLFW's
-																						// window) that has been used to draw in during this iteration and outputs to screen
-		glfwPollEvents();																// Checks if any events are triggered (i.e keyboard input or mouse movement), 
-																						// updates window state and calls appropriate callback methods
+		glfwSwapBuffers(pWindow);																		// Swaps the color buffer (large buffer that contains color values for each pixel in GLFW's
+																																// window) that has been used to draw in during this iteration and outputs to screen
+		glfwPollEvents();																						// Checks if any events are triggered (i.e keyboard input or mouse movement), 
+																																// updates window state and calls appropriate callback methods
 	}
 	
 	// ---------- Clean up ----------
